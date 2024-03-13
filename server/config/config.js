87947cdfbbc35cd5e2042b8213924eb3e5188ee6
Mathew Tomy <159645212+Mathew-Tomy@users.noreply.github.com>
2024-03-13 19:30:41 -0400

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,14 +2,13 @@ const TABLE_NAME = 'market_place';
 const config = {
     PORT: process.env.PORT || 5000,
     DB_CONNECTION: `mongodb://localhost/${TABLE_NAME}`,
-    // DB_CONNECTION: `mongodb+srv://iva2:iveto1234@cubicle.qzem5.mongodb.net/allForYou?retryWrites=true&w=majority`,
-    SECRET: 'badumts',
+    SECRET: '',
     SALT: 10,
     COOKIE_NAME: 'USER_SESSION',
-    CLOUDINARY_NAME: 'dym6r4jiz',
-    CLOUDINARY_API_KEY: 621713157117422,
-    CLOUDINARY_API_SECRET: 'RmC1YhPf4TldMGE8U1WwHqWD-jk',
-    CLOUDINARY_STORAGE: 'pza5zln6'
+    CLOUDINARY_NAME: '',
+    CLOUDINARY_API_KEY: ,
+    CLOUDINARY_API_SECRET: '',
+    CLOUDINARY_STORAGE: ''
 }
 
 module.exports = config;
